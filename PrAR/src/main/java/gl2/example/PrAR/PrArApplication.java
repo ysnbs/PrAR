c0831e053732c8d5facf67e947e929930a94a48e
Mohamed Yassin Ben slama <yassinbenslama13@gmail.com>
2026-04-29 18:25:45 +0100

    initialisation de projet

---
+++ PrAR/src/main/java/gl2/example/PrAR/PrArApplication.java
@@ -0,0 +1,13 @@
+package gl2.example.PrAR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrArApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrArApplication.class, args);
+	}
+
+}
---
+++ PrAR/src/test/java/gl2/example/PrAR/PrArApplicationTests.java
@@ -0,0 +1,13 @@
+package gl2.example.PrAR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrArApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
